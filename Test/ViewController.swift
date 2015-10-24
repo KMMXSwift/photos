fb1c21ebd1be3e1cc815205895f2681ace62f763
Ricardo Michel Reyes <me@richiereyes.com>
2015-10-24 10:20:04 -0500

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Ricardo Michel Reyes Martínez on 10/24/15.
+//  Copyright © 2015 KMMX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
